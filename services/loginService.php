dcbf942206d42313d68263f4e1a2f65ada740207
Braian-paC <braianbbg4@gmail.com>
2026-04-01 19:27:29 -0300

    Alterando log error loginService.php

--- services/loginService.php
+++ services/loginService.php
@@ -2,8 +2,9 @@
 session_start();
 
 // Para ver o erro que está acontecendo.
-ini_set('display_errors', 1);
-error_reporting(E_ALL);
+ini_set('display_errors', 0);
+ini_set('log_errors', 1);
+ini_set('error_log', __DIR__.'/../logs/php-error.log');
 
 // Conexão ao Banco de Dados.
 include __DIR__ . "/../config/database.php";
