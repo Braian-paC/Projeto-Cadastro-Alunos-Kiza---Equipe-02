bd1933508be655dd8a37f7fba3a179e93387342b
Braian-paC <braianbbg4@gmail.com>
2026-04-01 19:43:34 -0300

    Tratando erro 500 Railway PT-2

--- config/database.php
+++ config/database.php
@@ -12,12 +12,18 @@ $usuario = getenv('DB_USER') ?: 'root';
 $senha = getenv('DB_PASS') ?: 'lpZdolCpdlLPRFDHpvooJbHkwPSVDRYV';
 $porta = getenv('DB_PORT') ?: '39659';
 
-mysqli_report(MYSQLI_REPORT_ERROR | MYSQLI_REPORT_STRICT);
+if (function_exists('mysqli_report')) {
+    mysqli_report(MYSQLI_REPORT_ERROR | MYSQLI_REPORT_STRICT);
+}
+
+if (!extension_loaded('mysqli')) {
+    throw new RuntimeException('Extensão mysqli não está disponível no servidor.');
+}
 
 $conn = new mysqli($hostname, $usuario, $senha, $bancodedados, $porta);
 
 if ($conn->connect_error) {
-    die("Erro de conexão: " . $conn->connect_error);
+    throw new RuntimeException('Erro de conexão: ' . $conn->connect_error);
 }
 
 $conn->set_charset('utf8mb4');
